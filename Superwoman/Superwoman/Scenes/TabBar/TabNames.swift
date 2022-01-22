3af233258f987ac0c54cbbc1eb6ba2624cb416ee
Silvia España <silviaespanagil@gmail.com>
2022-01-22 21:22:34 +0100

    Add an enum to manage tabnames

---
+++ Superwoman/Superwoman/Scenes/TabBar/TabNames.swift
@@ -0,0 +1,15 @@
+//
+//  TabNames.swift
+//  Superwoman
+//
+//  Created by Silvia España on 22/1/22.
+//
+
+import Foundation
+
+enum TabNames: Hashable {
+    
+    case dashboard
+    case stakeholders
+    case projects
+}
