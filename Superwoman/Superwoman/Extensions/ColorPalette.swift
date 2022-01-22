2c355b3b6d80f2b5c248b37fda2ef83ecdcb13d2
Silvia España <silviaespanagil@gmail.com>
2022-01-22 11:42:19 +0100

    Set up an initial color palette

---
+++ Superwoman/Superwoman/Extensions/ColorPalette.swift
@@ -0,0 +1,16 @@
+//
+//  ColorPalette.swift
+//  Superwoman
+//
+//  Created by Silvia España on 22/1/22.
+//
+
+import SwiftUI
+
+public extension Color {
+    
+    static let lightPink = Color("LightPink")
+    static let lightBlue = Color("LightBlue")
+    static let coral = Color("Coral")
+    static let darkBlue = Color("DarkBlue")
+}
