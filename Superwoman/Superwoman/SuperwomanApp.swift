4de1c9e63e38e5db01df95ee7ea11ba9d6834441
Silvia España <silviaespanagil@gmail.com>
2022-01-22 11:06:44 +0100

    Repository setup - initial commit

---
+++ Superwoman/Superwoman/SuperwomanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperwomanApp.swift
+//  Superwoman
+//
+//  Created by Silvia España on 22/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SuperwomanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
