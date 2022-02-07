94705d8c8e15bfe3c5dc0dc0bf1700fdebf3ae91
Silvia España <silviaespanagil@gmail.com>
2022-02-07 09:44:03 +0100

    Create DB entities and fix previous entities

---
+++ Superwoman/Superwoman/Entities/Entities/FundedProjects.swift
@@ -0,0 +1,14 @@
+//
+//  FundedProjects.swift
+//  Superwoman
+//
+//  Created by Silvia España on 31/1/22.
+//
+
+import Foundation
+
+struct FundedProject: Identifiable {
+    
+    let id: UUID
+    var name: String
+}
