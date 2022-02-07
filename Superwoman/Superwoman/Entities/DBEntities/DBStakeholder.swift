613599553480eb963af2adef37ab3dc927df9058
Silvia España <silviaespanagil@gmail.com>
2022-02-07 13:53:33 +0100

    Add DBEntities

---
+++ Superwoman/Superwoman/Entities/DBEntities/DBStakeholder.swift
@@ -0,0 +1,32 @@
+//
+//  DBStakeholder.swift
+//  Superwoman
+//
+//  Created by Silvia España on 22/1/22.
+//
+//
+//import Foundation
+//import CoreData
+//
+//extension DBStakeholder {
+//    
+//    func convertToEntity() -> Stakeholder {
+//        
+//        var fundProj: [FundedProject] = []
+//        
+//        if let fundedProjects = fundedProjects {
+//            fundProj = fundedProjects.map({ $0.convertToEntity()})
+//        }
+//        if let fundedProjects = fundedProjects, let dbFundedProjects = fundedProjects.allObjects as? [DBFundedProject] {
+//
+//            fundProj = dbFundedProjects.map({ $0.convertToEntity()})
+//        }
+//        
+//        return Stakeholder(id: UUID(),
+//                           fullName: fullName ?? "",
+//                           email: email ?? "",
+//                           website: website ?? "",
+//                           fundedProjects: fundProj,
+//                           fundedAmount: Int(fundedAmount))
+//    }
+//}
