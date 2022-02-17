bd364acd12ca743b33d1a55f3a2025fe1c85634e
Silvia España <silviaespanagil@gmail.com>
2022-02-17 11:25:18 +0100

    Uodate dashboard view model to add getStakeholder method

--- Superwoman/Superwoman/Scenes/Dashboard/DashboardViewModel.swift
+++ Superwoman/Superwoman/Scenes/Dashboard/DashboardViewModel.swift
@@ -10,8 +10,15 @@ import SwiftUI
 
 class DashboardViewModel: ObservableObject {
     
+    @Published public private(set) var stakeholders: [Stakeholder] = []
+    
     let columns = [GridItem(.flexible()),
                    GridItem(.flexible())]
     let spacing: CGFloat = 20
+    
+    func getStakeholders() {
+        
+        stakeholders = GetStakeholdersUseCase().execute()
+    }
     
 }
